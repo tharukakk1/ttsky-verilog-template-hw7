b0d17a4b2a1fc54cf6e87013763240da317b1373
Tharuka Kodituwakku <106564811+tharukakk1@users.noreply.github.com>
2026-03-12 22:59:39 -0700

    copying project.v from hw6

--- src/project.v
+++ src/project.v
@@ -16,12 +16,17 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // // All output pins must be assigned. If not used, assign to 0.
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
+  // // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+  
+  wire [8:0] full_result = ui_in + uio_in;
+  assign uo_out  = full_result[7:0];
+  assign uio_out = {7'b0, full_result[8]};
+  assign uio_oe  = 8'b00000001;
 
 endmodule
