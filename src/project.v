2eb7fcb366778c58cbb6906378ba8e669bb89a7e
Tharuka Kodituwakku <106564811+tharukakk1@users.noreply.github.com>
2026-03-12 23:18:30 -0700

    updated to DFF
    Refactor module inputs and outputs, add state register.

--- src/project.v
+++ src/project.v
@@ -6,27 +6,29 @@
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // // All output pins must be assigned. If not used, assign to 0.
-  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uio_out = 0;
-  // assign uio_oe  = 0;
+  reg q;
 
-  // // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-  
-  wire [8:0] full_result = ui_in + uio_in;
-  assign uo_out  = full_result[7:0];
-  assign uio_out = {7'b0, full_result[8]};
-  assign uio_oe  = 8'b00000001;
+  always @(posedge clk) begin
+    if (!rst_n)        // rst_n is active-low
+      q <= 1'b0;
+    else if (ui_in[1]) // en_i
+      q <= ui_in[0];   // d_i
+  end
+
+  assign uo_out  = {7'b0, q};
+  assign uio_out = 8'b0;
+  assign uio_oe  = 8'b0;
+
+  wire _unused = &{ena, ui_in[7:2], uio_in, 1'b0};
 
 endmodule
